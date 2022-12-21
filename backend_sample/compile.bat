4861b157962d59cbf336f00c6de76ff0ea2ae540
SeungHyeokJeon <sdkazx2717@gmail.com>
2022-12-21 10:37:59 +0900

    delete back sample

--- backend_sample/compile.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-Set app_name=restsample
-FOR %%i IN (init,build,run) DO ( IF %%i==run ( "%JAVA_HOME%\bin\java.exe" -jar  ./build/libs/%app_name%-0.0.1-SNAPSHOT.jar ) ELSE ( .\gradlew %%i ) )
